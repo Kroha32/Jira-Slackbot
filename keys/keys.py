64c80dec6434359847ee49bff34c34ed4684e7b7
Kroha32 <69897822+Kroha32@users.noreply.github.com>
2020-08-19 14:03:36 +0300

    Add files via upload

---
+++ keys/keys.py
@@ -0,0 +1,7 @@
+slack_token ="xoxb**********************"
+curlic = "link to jira"
+data = {"os_username":"login_bota", "os_password":"parol_bota"}
+url = "link na login testovoi jiri"
+data2 = {"os_username":"same", "os_password":"same"}
+url2 = "ssila na login jiri"
+adm_ch='G017LGAV0HH'//номер канала в слаке для админов
