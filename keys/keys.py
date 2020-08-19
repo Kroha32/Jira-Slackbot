70fd47a1cb44ea797f0b2bcf03955567aa5df96a
Kroha32 <69897822+Kroha32@users.noreply.github.com>
2020-08-19 14:04:35 +0300

    Update keys.py

--- keys/keys.py
+++ keys/keys.py
@@ -1,7 +1,7 @@
 slack_token ="xoxb**********************"
 curlic = "link to jira"
 data = {"os_username":"login_bota", "os_password":"parol_bota"}
-url = "link na login testovoi jiri"
+url = "link na login test jira"
 data2 = {"os_username":"same", "os_password":"same"}
-url2 = "ssila na login jiri"
+url2 = "link na login jira"
 adm_ch='G017LGAV0HH'//номер канала в слаке для админов
